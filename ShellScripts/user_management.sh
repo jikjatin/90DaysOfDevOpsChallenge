6d5fa78b18e3ba86475e945b5353524635b321ba
jikjatin <jatinrajput04@gmail.com>
2025-03-02 03:35:37 +0530

    complete user management script

--- ShellScripts/user_management.sh
+++ ShellScripts/user_management.sh
@@ -1,27 +1,79 @@
 #!/bin/bash
 <<info
-This script create new users and print verification message at the end
+This script create provides various user management functions like create,delete,list
 info
 
 
+#-------User creation-------
+
 function create_user(){
-  read -p "Enter username: " uname
-  if [ "$uname" == "$(grep $uname /etc/passwd | awk -F: '{print $1}')" ]
+  read -p "Enter username to create: " uname
+  if [ "$uname" == "$(grep $uname /etc/passwd | awk -F: '{print $1}')" ] 
   then
     echo "<<====User $uname already exist====>>"
   else
     read -p "Enter user password: " pword
     sudo useradd $uname
-    echo -e "$pword\n$pword" | sudo passwd "$uname" > /dev/null 2>&1
+    echo -e "$pword\n$pword" | sudo passwd "$uname" > /dev/null 2>&1   #give password and password in next line(-e) as input to passwd command
     echo "<<====User $uname created successfuly====>>"
   fi
 }
-read -p "Type 'c' to create new user: " input
-if [ "c" == "$input" ]
+
+#--------User deletion--------
+
+function user_delete(){
+  read -p "Enter username to delete: " uname
+  if [ "$uname" == "$(grep $uname  /etc/passwd | awk -F: '{print $1}')" ]
+  then
+    sudo userdel -r $uname > /dev/null 2>&1
+    echo "<<====User $uname deleted successfully====>>"
+  else
+    echo "<<====User $uname doesn't exist====>>"
+  fi
+}
+
+#-------Password reset-------
+
+function passwd_reset(){
+  read -p "Enter username for password reset: " uname
+  if [ "$uname" == "$(grep $uname  /etc/passwd | awk -F: '{print $1}')" ]
+  then
+    read -p "Enter new password: " pword
+    echo -e "$pword\n$pword" | sudo passwd "$uname" > /dev/null 2>&1
+  else
+    echo "<<====User $uname doesn't exist====>>"
+  fi
+}
+
+#-------List all user--------
+function list_user(){
+cat /etc/passwd | awk -F: '{print $1" "$3}'
+}
+
+#------Help guide-----------
+function help(){
+echo -e "Use '-c' argument to create a new user\nUse '-d' argument to delete a user\nUse '-r' argument to reset password of a user\nUse '-l' argument to list all user along with their UID"
+}
+
+
+if [ "$1" == "-c" ]
 then
   create_user
+elif [ "$1" == "-d" ] 
+then
+  user_delete
+elif [ "$1" == "-r" ]
+then
+  passwd_reset
+elif [ "$1" == "-l" ]
+then
+  list_user
+elif [ "$1" == "-h" ]
+then
+  help
+else
+  echo "Use '-h' argument for help"
 fi
-
 
 
 <<info
