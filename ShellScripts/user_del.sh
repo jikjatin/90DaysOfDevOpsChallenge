6502740c7ac6c595e2c0e7db6eca02cc17229a1c
jikjatin <jatinrajput04@gmail.com>
2025-03-02 01:38:01 +0530

    created user delete script

---
+++ ShellScripts/user_del.sh
@@ -0,0 +1,28 @@
+<<info
+This script delete a user and print verification message at the end
+info
+
+
+
+function user_delete(){
+read -p "Enter username to delete: " uname
+  if [ "$uname" == "$(grep $uname  /etc/passwd | awk -F: '{print $1}')" ]
+  then
+    sudo userdel -r $uname > /dev/null 2>$#
+    echo "<<====User $uname deleted successfully====>>"
+  else
+    echo "<<====User $uname doesn't exist====>>"
+  fi
+}
+
+read -p "Type 'd' to delete a user: " input
+if [ "d" == "$input" ]
+then
+  user_delete
+fi
+
+
+
+<<info
+/dev/null 2>$#  '2>&1' is still showing 1 error message somehow using "$#" solved it but i need more explainnation on this. Will research later
+info 
