6d380d5486685bd096f7987594f580240f276af0
jikjatin <jatinrajput04@gmail.com>
2025-03-02 01:53:54 +0530

    Revert "updated"
    This reverts commit 7e2a3772b3e7d8db79895c735e13737194e5cfaf.

--- ShellScripts/user_del.sh
+++ ShellScripts/user_del.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 <<info
 This script delete a user and print verification message at the end
 info
@@ -9,7 +8,7 @@ function user_delete(){
 read -p "Enter username to delete: " uname
   if [ "$uname" == "$(grep $uname  /etc/passwd | awk -F: '{print $1}')" ]
   then
-    sudo userdel -r $uname > /dev/null 2>&1
+    sudo userdel -r $uname > /dev/null 2>$#
     echo "<<====User $uname deleted successfully====>>"
   else
     echo "<<====User $uname doesn't exist====>>"
@@ -21,3 +20,9 @@ if [ "d" == "$input" ]
 then
   user_delete
 fi
+
+
+
+<<info
+/dev/null 2>$#  '2>&1' is still showing 1 error message somehow using "$#" solved it but i need more explainnation on this. Will research later
+info 
