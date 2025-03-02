2ffba7f2f76059c33d7339f502dd5c6dd70021a7
jikjatin <jatinrajput04@gmail.com>
2025-03-02 21:19:21 +0530

    backup script completed

--- ShellScripts/backup_with_rotation.sh
+++ ShellScripts/backup_with_rotation.sh
@@ -1,7 +1,24 @@
-#!bin/bash
-src=$1
-dest=/home/Jatin/Backups
-timestamp=$(date '+%y-%m-%d-%s')
+#!/bin/bash
 
-cp -r $src $dest/backup_$timestamp/
-echo "Backup completed $dest/backup_$timestamp"
+dest=/home/Jatin/Backups
+src=$1
+
+function log_create(){
+  timestamp=$(date '+%y-%m-%d-%s')
+  mkdir $dest/backup_$timestamp && cp -r $src $dest/backup_$timestamp/
+  echo "Backup completed $dest/backup_$timestamp"
+}
+
+function log_rotate(){
+  rm -rf ${backup_arr[$backup_no-1]} 
+}
+
+
+log_create
+backup_no=$(ls -dt $dest/backup*/ | wc | awk '{print$2}') #'-dt' list all directory sort by newest first
+backup_arr=($(ls -dt $dest/backup*/)) #created an array which store all backup folder names in order of latest to oldest
+if [ "$backup_no" -gt "3" ]
+then
+  log_rotate
+  backup_no=$(ls -dt $dest/backup*/ | wc | awk '{print$2}')
+fi
