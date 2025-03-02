9de5edf4bcc597d1f13c86e6fc502a1895016543
jikjatin <jatinrajput04@gmail.com>
2025-03-02 11:17:33 +0530

    added log script

--- ShellScripts/backup_with_rotation.sh
+++ ShellScripts/backup_with_rotation.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 src=$1
 dest=/home/Jatin/Backups
-timestamp=$(date '+%y-%m-%d')
+timestamp=$(date '+%y-%m-%d-%s')
 
 cp -r $src $dest/backup_$timestamp/
 echo "Backup completed $dest/backup_$timestamp"
